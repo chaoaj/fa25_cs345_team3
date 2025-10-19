3756a78bd02827369469840cbce98cf0d74315fc
Joseph Tessitore <tessitjd@dukes.jmu.edu>
2025-10-19 18:59:40 -0400

    "Implement" a basic tower

--- main/sketch.js
+++ main/sketch.js
@@ -1,9 +1,37 @@
+// Javascript is weird about modules sometimes? This works tho
+var BasicTower;
+function preload() {
+  import("./towers/basic_tower.js").then(m => {
+    BasicTower = m;
+  });
+}
+
+let towers = [];
+
 function setup() {
   createCanvas(400, 400);
+
+  // Test tower
+  const tower = new Tower(BasicTower.draw, BasicTower.update);
+  tower.obj = {
+    position: {
+      x: 300,
+      y: 300,
+    },
+  };
+  towers.push(tower);
 }
+
+
 
 function draw() {
   background(220);
+  for (let tower of towers) {
+    tower.update();
+  }
+  for (let tower of towers) {
+    tower.draw();
+  }
 }
 
 
@@ -12,36 +40,40 @@ function draw() {
 
 // yet to be implemented. takes a position, returns the nearest car
 // object, whatever that looks like.
-function getNearestCar(x, y)
+function getNearestCar(x, y) {
+  
+}
 // Tower constructor. could be called like new Tower(whatever)
-function Tower(draw, update, obj) {
-    // called every frame to draw the tower on the screen maybe takes
-    // a position, or maybe uses the position stored in the object
-    this.draw = draw;
-    // runs the actual game code, called every frame.
-    //
-    // for the basic tower, this would probably get a timestamp, and
-    // compare it to a local variable lastFiredTimestamp. if the
-    // difference is greater than a second, call getNearestCar. If its
-    // within a certain radius, fire a new Projectile() with the
-    // position of the tower, and with a velocity pointing towards the
-    // car. then, update lastFiredTimestamp to the current timestamp,
-    // so that a second or so will be waited again.
-    this.update = update;
-    // local data for this tower; varies between towers; composition
-    // over inheritance!
-    this.obj = obj;
-    // Other possible properties:
-    //  • position	- (x,y) coordinates
-    //  • cost		- how much it costs to place it
-    //  • upgrades	- some structure for the upgrades?
-    //  • name          - what type of tower it is. might be used somewhere
-    //  • isGhost       - while a tower is being placed, it is shown on the
-    //                    screen, but it shouldn't fire at anything. if this
-    //                    is true, then the tower doesn't actually exist,
-    //                    and shouldn't fire or anything.
-    // would have to have other code implemented to know what we need.
+function Tower(draw, update) {
+  // called every frame to draw // TODO: he tower on the screen maybe takes
+  // a position, or maybe uses the position stored in the object
+  this.draw = draw;
+  // runs the actual game code, called every frame.
+  //
+  // for the basic tower, this would probably get a timestamp, and
+  // compare it to a local variable lastFiredTimestamp. if the
+  // difference is greater than a second, call getNearestCar. If its
+  // within a certain radius, fire a new Projectile() with the
+  // position of the tower, and with a velocity pointing towards the
+  // car. then, update lastFiredTimestamp to the current timestamp,
+  // so that a second or so will be waited again.
+  this.update = update;
+  // local data for this tower; varies between towers; composition
+  // over inheritance!
+  this.obj = {};
+  // Other possible properties:
+  //  • position	- (x,y) coordinates
+  //  • cost		- how much it costs to place it
+  //  • upgrades	- some structure for the upgrades?
+  //  • name          - what type of tower it is. might be used somewhere
+  //  • isGhost       - while a tower is being placed, it is shown on the
+  //                    screen, but it shouldn't fire at anything. if this
+  //                    is true, then the tower doesn't actually exist,
+  //                    and shouldn't fire or anything.
+  // would have to have other code implemented to know what we need.
 }
 
 // A function to place a tower at a position.
-function placeTower(tower, x, y);
+function placeTower(tower, x, y) {
+  
+}
---
+++ main/towers/basic_tower.js
@@ -0,0 +1,36 @@
+// Cars aren't implemented yet, so this temporarily returns the mouse position
+function getNearestCar(x, y) {
+  return {
+    x: mouseX,
+    y: mouseY,
+  };
+}
+
+const bodyColor = [255, 0, 0];
+const turretColor = [200, 200, 200];
+// How many pixels away the tower can shoot
+const firingRange = 25;
+
+export function update() {
+  this.obj.target =  getNearestCar(this.obj.position.x, this.obj.position.y);
+  // TODO: Add some way of getting a timestamp so this can have a cooldown
+  const enoughTimeElapsed = true;
+  // Check whether the nearest car is close enough.
+  const distanceToTarget = dist(this.obj.target.x, this.obj.target.y, this.obj.position.x, this.obj.position.y)
+  if (enoughTimeElapsed && distanceToTarget < firingRange) {
+    // TODO: Fire a projectile once those are implemented
+    console.log("Fire!")
+  }
+}
+
+export function draw() {
+  stroke(0, 0, 0);
+  fill(...bodyColor);
+  circle(this.obj.position.x, this.obj.position.y, 25);
+  // Point the turret at the mouse
+  translate(this.obj.position.y, this.obj.position.x);
+  rotate(Math.atan2(this.obj.target.y - this.obj.position.y, this.obj.target.x - this.obj.position.x));
+  fill(...turretColor);
+  rect(-5, -5, 30, 10);
+  resetMatrix();
+}
