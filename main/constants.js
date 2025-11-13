7a0834b21fdeaeaa3401a59c27a87e07cc6c662f
Joseph Tessitore <tessitjd@dukes.jmu.edu>
2025-11-13 16:17:06 -0500

    You can now remove a tower

--- main/constants.js
+++ main/constants.js
@@ -2,3 +2,8 @@ export const mapWidth = 640;
 export const mapHeight = 480;
 export const menuWidth = 200;
 export const menuBorderPadding = 10;
+
+// Tower menu information
+export const towerMenuWidth = 100;
+export const towerMenuHeight = 100;
+export const towerMenuCloseButtonSize = 48;
--- main/sketch.js
+++ main/sketch.js
@@ -21,6 +21,10 @@ let spawnTimer = 0; //Countdown time for next spawn
 let mapImage;
 let placeImage;
 
+// Tower menu coordinates
+let tmenuX;
+let tmenuY;
+
 // Whether a coordinate is in bounds of the screen
 function inBounds(x, y) {
   return x >= 0 && x <= Constants.mapWidth && y >= 0 && y <= Constants.mapHeight;
@@ -57,6 +61,7 @@ function canPlaceAt(x, y) {
 
 //state variable for tower being dragged
 let towerBeingPlaced = null;
+let towerBeingMenued = null;
 
 // --- NEW: Data-driven array for the tower menu ---
 // To add a new tower, just add a new object to this array.
@@ -65,7 +70,7 @@ const towerMenu = [
     name: "Basic Tower",
     cost: 0,
     // This function creates the actual tower object
-    create: () => new Tower(BasicTower.draw, BasicTower.update),
+    create: (name) => new Tower(BasicTower.draw, BasicTower.update, name),
     // This function draws the icon in the menu
     drawIcon: (x, y) => {
       push();
@@ -78,8 +83,7 @@ const towerMenu = [
       pop();
     },
     // These will be populated by the draw() loop
-    menuX: 0,
-    menuY: 0,
+    menuPos: null,
     menuSize: 40 // The click-able radius
   },
   // --- EXAMPLE: Add a new tower here ---
@@ -253,10 +257,12 @@ export function draw() {
     let row = Math.floor(index / 2);
 
     // Calculate and store the button's position
-    let x = (col === 0) ? col1X : col2X;
-    let y = startY + row * buttonSpacing;
-    towerType.menuX = x;
-    towerType.menuY = y;
+    
+    // TODO: put this in setup somehow, because it doesn't need to be
+    // recalculated every frame.
+    const x = (col === 0) ? col1X : col2X;
+    const y = startY + row * buttonSpacing;
+    towerType.menuPos = createVector(x, y);
 
     // Draw the button icon
     towerType.drawIcon(x, y);
@@ -267,6 +273,22 @@ export function draw() {
     textAlign(CENTER, CENTER);
     text(`$${towerType.cost}`, x, y + 25);
   });
+  
+  if (towerBeingMenued) {
+    tmenuX = towerBeingMenued.obj.position.x + 20;
+    tmenuY = towerBeingMenued.obj.position.y;
+
+    // Make sure that the menu doesn't go below the screen
+    tmenuY = Math.min(tmenuY, Constants.mapHeight - Constants.towerMenuHeight - 10);
+    fill('tan');
+    rect(tmenuX, tmenuY, Constants.towerMenuWidth, Constants.towerMenuHeight);
+    fill(0, 0, 0);
+    textAlign(LEFT, TOP);
+    text(`${towerBeingMenued.name}`, tmenuX, tmenuY);
+    fill(255, 0, 0);
+    // TODO: make some sort of button asset for this :3
+    rect(tmenuX + 10, tmenuY + Constants.towerMenuHeight - Constants.towerMenuCloseButtonSize - 10, Constants.towerMenuCloseButtonSize, Constants.towerMenuCloseButtonSize);
+  }
 
   // Draw the lines if debug mode is on
   if (DEBUG) {
@@ -312,13 +334,16 @@ export function getNearestCar(x, y) {
 
 //Function to add mouse pressed functionality
 export function mousePressed() {
-  if (towerBeingPlaced !== null) {
-    if (canPlaceAt(mouseX, mouseY)) {
+  const mouseVector = createVector(mouseX, mouseY);
+  // If there is a tower being placed, then clicking the mouse should
+  // place the tower
+  if (towerBeingPlaced) {
+    if (canPlaceAt(mouseVector.x, mouseVector.y)) {
       towerBeingPlaced.obj.isGhost = false;
-      towerBeingPlaced.obj.position = createVector(mouseX, mouseY);
+      towerBeingPlaced.obj.position = mouseVector.copy();
       towers.push(towerBeingPlaced);
       towerBeingPlaced = null;
-    } else if (mouseX >= 640) {
+    } else if (mouseVector.x >= Constants.mapWidth) {
       // Remove ghost tower if the tower is being placed back in the
       // menu.
       towerBeingPlaced = null;
@@ -326,21 +351,70 @@ export function mousePressed() {
     return;
   }
 
+  let somethingClicked = false;
+
+  // Check for a click to the menu TODO: It'd be neat if checking if a
+  // vector collided with a rectangle (AABB) was its own function,
+  // because i just wrote it twice.
+  if (mouseX >= tmenuX
+      && mouseX < tmenuX + Constants.towerMenuWidth
+      && mouseY >= tmenuY
+      && mouseY < tmenuY + Constants.towerMenuHeight) {
+
+    somethingClicked = true;
+    // Check if the close button was clicked
+
+    // TODO: Duplicated logic for placing the menu and clicking the
+    // menu WILL cause a bug at some point.
+    const beginX = tmenuX + 10;
+    const endX = beginX + Constants.towerMenuCloseButtonSize;
+    const beginY = tmenuY + Constants.towerMenuHeight - Constants.towerMenuCloseButtonSize - 10;
+    const endY = beginY + Constants.towerMenuCloseButtonSize;
+    if (mouseX >= beginX
+        && mouseX < endX
+        && mouseY >= beginY
+        && mouseY < endY) {
+      // Jankily remove the tower
+      towers = towers.filter(function (t) {
+        return t != towerBeingMenued;
+      })
+      towerBeingMenued = null;
+    }
+  }
+
+  // Check if a tower in the tower purchase menu was clicked
   for (const towerType of towerMenu) {
-    if (dist(mouseX, mouseY, towerType.menuX, towerType.menuY) < towerType.menuSize / 2) {
-      towerBeingPlaced = towerType.create();
+    if (mouseVector.dist(towerType.menuPos) < towerType.menuSize / 2) {
+      towerBeingPlaced = towerType.create(towerType.name);
       towerBeingPlaced.obj = {
         position: createVector(mouseX, mouseY),
         isGhost: true
       };
+      somethingClicked = true;
       break;
     }
+  }
+
+  // If nothing was clicked so far, check if any tower was clicked
+  if (!somethingClicked) {
+    for (let tower of towers) {
+      //                                         TODO: get rid of this magic number
+      if (mouseVector.dist(tower.obj.position) < 30) {
+        towerBeingMenued = tower;
+        somethingClicked = true;
+        break;
+      }
+    }
+  }
+  // If nothing was clicked, close the tower popup menu
+  if (!somethingClicked) {
+    towerBeingMenued = null;
   }
 }
 
 
 // Tower constructor. could be called like new Tower(whatever)
-function Tower(draw, update) {
+function Tower(draw, update, name) {
   // called every frame to draw // TODO: he tower on the screen maybe takes
   // a position, or maybe uses the position stored in the object
   this.draw = draw;
@@ -354,6 +428,7 @@ function Tower(draw, update) {
   // car. then, update lastFiredTimestamp to the current timestamp,
   // so that a second or so will be waited again.
   this.update = update;
+  this.name = name;
   // local data for this tower; varies between towers; composition
   // over inheritance!
   this.obj = {};
